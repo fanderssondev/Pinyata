6db1b3e4138f261454cf62abf882b90d3e528a9d
Fredrik <fredrikanderssondev@gmail.com>
2024-06-07 14:31:01 +0200

    Initial project setup

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Logo from '$lib/components/Logo.svelte';
+	import '../app.css';
+</script>
+
+<header class="h-6 w-6">
+	<Logo />
+</header>
+
+<slot />
+
+<style>
+	/* svg {
+		width: 25px;
+		height: 25px;
+	} */
+</style>
